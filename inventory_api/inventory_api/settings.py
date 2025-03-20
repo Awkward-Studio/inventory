ef8e7053940de04d6b6a4b66ff75e2088e7f505d
Atique Khan <atique.khan@zeza.tech>
2025-03-20 13:40:31 +0530

    Update ALLOWED_HOSTS to include production domain

--- inventory_api/inventory_api/settings.py
+++ inventory_api/inventory_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-xq06m4#wnz3so71a4l@=ida2wg@yd$a@6l(84165m5^t71%m)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["inventory-6p2y.onrender.com"]
 
 
 # Application definition
