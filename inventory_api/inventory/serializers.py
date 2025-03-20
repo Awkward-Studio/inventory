20bd01c5b47b99cdefc697186daa0ffd916b9fde
Atique Khan <atique.khan@zeza.tech>
2025-03-20 11:25:15 +0530

    Update ProductMediaSerializer to include all fields

--- inventory_api/inventory/serializers.py
+++ inventory_api/inventory/serializers.py
@@ -9,7 +9,7 @@ class ProductMediaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductMedia
-        fields = ["id", "product", "media_type", "appwrite_file_id", "created_at"]
+        fields = "__all__"
         read_only_fields = ["id", "created_at"]  # Prevent modification of these fields
 
 
